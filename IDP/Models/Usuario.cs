816f3a8bf48a1300df3498cdf513c1762aa69deb
t2s-bruno <t2s-bruno@LAPTOP-0E99B8TI>
2022-01-28 23:44:55 -0300

    Adicionar arquivos de projeto.

---
+++ IDP/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IDP.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public Usuario(int id, string login, string senha)
+        {
+            Id = id;
+            Login = login;
+            Senha = senha;
+        }
+
+        [Key]
+        [Column("usua_id")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Login obrigatório")]
+        [Column("usua_login")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Senha obrigatória")]
+        [Column("usua_senha")]
+        public string Senha { get; set; }
+    }
+}
